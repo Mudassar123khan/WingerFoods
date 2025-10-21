ea71ff60e6897ab369b14d8820b5f0e653036200
Mudassir Khan <12mudassarkhan@gmail.com>
2025-10-22 00:19:11 +0530

    The frontend of the project is now some what ready but not complete.

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,359 @@
+import logo from './logo.png'
+import add_icon from './add_icon.png'
+import order_icon from './order_icon.png'
+import profile_image from './profile_image.png'
+import upload_area from './upload_area.png'
+import parcel_icon from './parcel_icon.png'
+import search_icon from './search_icon.png'
+import basket_icon from './basket_icon.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.png'
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.png'
+import food_8 from './food_8.png'
+import food_9 from './food_9.png'
+import food_10 from './food_10.png'
+import food_11 from './food_11.png'
+import food_12 from './food_12.png'
+import food_13 from './food_13.png'
+import food_14 from './food_14.png'
+import food_15 from './food_15.png'
+import food_16 from './food_16.png'
+import food_17 from './food_17.png'
+import food_18 from './food_18.png'
+import food_19 from './food_19.png'
+import food_20 from './food_20.png'
+import food_21 from './food_21.png'
+import food_22 from './food_22.png'
+import food_23 from './food_23.png'
+import food_24 from './food_24.png'
+import food_25 from './food_25.png'
+import food_26 from './food_26.png'
+import food_27 from './food_27.png'
+import food_28 from './food_28.png'
+import food_29 from './food_29.png'
+import food_30 from './food_30.png'
+import food_31 from './food_31.png'
+import food_32 from './food_32.png'
+import rating_starts from './rating_starts.png'
+
+export const assets ={
+    logo,
+    add_icon,
+    order_icon,
+    profile_image,
+    upload_area,
+    parcel_icon,
+    search_icon,
+    basket_icon,
+    rating_starts,
+}
+
+export const url = 'http://localhost:4000'
+
+export const menu_list = [
+    {
+        menu_name:"Salad",
+        menu_image:menu_1
+    },
+    {
+        menu_name:"Rolls",
+        menu_image:menu_2
+    },
+    {
+        menu_name:"Deserts",
+        menu_image:menu_3
+    },
+    {
+        menu_name:"Sandwich",
+        menu_image:menu_4
+    },
+    {
+        menu_name:"Cake",
+        menu_image:menu_5
+    },
+    {
+        menu_name:"Pure Veg",
+        menu_image:menu_6
+    },
+    {
+        menu_name:"Pasta",
+        menu_image:menu_7
+    },
+    {
+        menu_name:"Noodles",
+        menu_image:menu_8
+    },
+]
+
+
+export const food_list=
+[
+    {
+        _id: "1",
+        name: "Asparagus & Egg Yolk Salad",
+        image: food_1,
+        price: 18,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Salad"
+    },
+    {
+        _id: "2",
+        name: "Mixed Green Salad",
+        image: food_2,
+        price: 14,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Salad"
+    },
+    {
+        _id: "3",
+        name: "Radicchio Walnut Salad",
+        image: food_3,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Salad"
+    },
+    {
+        _id: "4",
+        name: "Grilled Halloumi Salad",
+        image: food_4,
+        price: 16,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Salad"
+    },
+    {
+        _id: "5",
+        name: "Spinach Lasagna Rolls",
+        image: food_5,
+        price: 20,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Pasta"
+    },
+    {
+        _id: "6",
+        name: "Chicken Tikka Roll",
+        image: food_6,
+        price: 11,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Rolls"
+    },
+    {
+        _id: "7",
+        name: "Chicken Kati Roll",
+        image: food_7,
+        price: 10,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Rolls"
+    },
+    {
+        _id: "8",
+        name: "Cheesy Stromboli",
+        image: food_8,
+        price: 13,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Stromboli"
+    },
+    {
+        _id: "9",
+        name: "Raspberry Ice Cream",
+        image: food_9,
+        price: 8,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Dessert"
+    },
+    {
+        _id: "10",
+        name: "Apple Mille-Feuille",
+        image: food_10,
+        price: 22,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Dessert"
+    },
+    {
+        _id: "11",
+        name: "Strawberry Panna Cotta",
+        image: food_11,
+        price: 10,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Dessert"
+    },
+    {
+        _id: "12",
+        name: "Vanilla Ice Cream",
+        image: food_12,
+        price: 7,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Dessert"
+    },
+    {
+        _id: "13",
+        name: "Pastrami Sandwich",
+        image: food_13,
+        price: 17,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Sandwich"
+    },
+    {
+        _id: "14",
+        name: "BLT Sandwich",
+        image: food_14,
+        price: 14,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Sandwich"
+    },
+    {
+        _id: "15",
+        name: "Chickpea Salad Sandwich",
+        image: food_15,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Sandwich"
+    },
+    {
+        _id: "16",
+        name: "Banh Mi Sandwich",
+        image: food_16,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Sandwich"
+    },
+    {
+        _id: "17",
+        name: "Cherry Cupcake",
+        image: food_17,
+        price: 5,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Cake"
+    },
+    {
+        _id: "18",
+        name: "Strawberry Cheesecake",
+        image: food_18,
+        price: 9,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Cake"
+    },
+    {
+        _id: "19",
+        name: "Berry Cream Cake",
+        image: food_19,
+        price: 18,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Cake"
+    },
+    {
+        _id: "20",
+        name: "Mango Cheesecake",
+        image: food_20,
+        price: 9,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Cake"
+    },
+    {
+        _id: "21",
+        name: "Stuffed Eggplant",
+        image: food_21,
+        price: 18,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Pure Veg"
+    },
+    {
+        _id: "22",
+        name: "Cauliflower Salad",
+        image: food_22,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Salad"
+    },
+    {
+        _id: "23",
+        name: "Caesar Salad",
+        image: food_23,
+        price: 14,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Salad"
+    },
+    {
+        _id: "24",
+        name: "Veggie Rice Bowl",
+        image: food_24,
+        price: 16,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Pure Veg"
+    },
+    {
+        _id: "25",
+        name: "Baked Ham Pasta",
+        image: food_25,
+        price: 19,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Pasta"
+    },
+    {
+        _id: "26",
+        name: "Fusilli Arrabbiata",
+        image: food_26,
+        price: 17,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Pasta"
+    },
+    {
+        _id: "27",
+        name: "Chicken Alfredo",
+        image: food_27,
+        price: 20,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Pasta"
+    },
+    {
+        _id: "28",
+        name: "Tuscan Chicken Pasta",
+        image: food_28,
+        price: 22,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Pasta"
+    },
+    {
+        _id: "29",
+        name: "Spaghetti with Ricotta",
+        image: food_29,
+        price: 21,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Pasta"
+    },
+    {
+        _id: "30",
+        name: "Spaghetti Marinara",
+        image: food_30,
+        price: 16,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Pasta"
+    },
+    {
+        _id: "31",
+        name: "Seafood Spaghetti",
+        image: food_31,
+        price: 24,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Pasta"
+    },
+    {
+        _id: "32",
+        name: "Pork Noodle Soup",
+        image: food_32,
+        price: 18,
+        description: "Food provides essential nutrients for overall health and well being",
+        category: "Noodles"
+    }
+]
